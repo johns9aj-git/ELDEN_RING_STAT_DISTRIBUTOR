1442fe21a9c28a5354e91d7c37a92d049e0ca232
ajohns2020 <johns9aj@mail.uc.edu>
2026-05-28 22:04:39 -0400

    Version 1.0 - Grabbing Values

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner; // Import the Scanner class to read user input
+public class Main {
+    public static void main(String[] args) {
+
+    // Intialization of the Scanner class to read user input from the console. 
+    // The program prompts the user to enter their total stat points and their class, then it displays the entered information back to the user.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your total stat points: ");
+        int totalStats = scanner.nextInt();
+        System.out.println("You have " + totalStats + " stat points.");
+        System.out.print("What class are you? (Hero, Bandit, Astrologer, Warrior, Prisoner, Confessor, Wretch or Vagabond):");
+        String playerClass = scanner.next();
+        System.out.println("You are a " + playerClass + ".");
+
+        // Generating random stats for the player based on their total stat points and class. The program uses a switch statement to assign different stat distributions based on the player's chosen class.
+        Random randStat = new Random();
+
+        int[] stats = new int[8]; // Assuming there are 8 stats to distribute
+        
+        for (int i = 0; i <totalStats; i++){
+            stats[randStat.nextInt(stats.length)]++; // Randomly distribute stat points
+        }
+        System.out.println("Your stats are:");
+        System.out.println("Vigor: " + stats[0]);
+        System.out.println("Mind: " + stats[1]);
+        System.out.println("Endurance: " + stats[2]);
+        System.out.println("Strength: " + stats[3]);
+        System.out.println("Dexterity: " + stats[4]);
+        System.out.println("Intelligence: " + stats[5]);
+        System.out.println("Faith: " + stats[6]);
+        System.out.println("Arcane: " + stats[7]);
+        
+
+
+    }
+}
